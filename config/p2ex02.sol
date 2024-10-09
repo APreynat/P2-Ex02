f055a2e6ddbfea278da0d5aadb28927e3d78f1b2
A PREYNAT <apreynat@gaming.tech>
2024-10-09 15:38:33 +0200

    init repo

---
+++ config/p2ex02.sol
@@ -0,0 +1,8 @@
+{
+    "solution_name": "p2ex02",
+    "format_version": "12.00",
+    "version": "16",
+    "version_full": "16.0.30319.14",
+    "minimum_version": "10.0.40219.1",
+    "projects": "P2-Ex02.prj"
+}
---
+++ src/P2-Ex02/main.cpp
@@ -0,0 +1,94 @@
+#include "pch.h"
+#include "main.h"
+
+//sf::RenderWindow* window;
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	WNDCLASSEXW wcex;
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style          = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc    = WndProc;
+	wcex.cbClsExtra     = 0;
+	wcex.cbWndExtra     = 0;
+	wcex.hInstance      = hInstance;
+	wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APP));
+	wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+	wcex.lpszMenuName   = nullptr;
+	wcex.lpszClassName  = L"WinAppClass";
+	wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+	if ( RegisterClassExW(&wcex)==0 )
+		return 0;
+
+	HWND hWnd = CreateWindowW(L"WinAppClass", L"Title", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+	if ( hWnd==NULL )
+		return 0;
+
+	//window = new sf::RenderWindow(hWnd);
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	sf::RenderWindow window(hWnd);
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	MSG msg;
+	while (window.isOpen()) {
+		while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+
+	}
+
+#ifdef _DEBUG
+	_CrtDumpMemoryLeaks();
+#endif
+
+	return 0;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+		case WM_COMMAND:
+		{
+			int id = LOWORD(wParam);
+			int notif = HIWORD(wParam);
+			return DefWindowProc(hWnd, message, wParam, lParam);
+			break;
+		}
+		case WM_PAINT:
+		{
+			PAINTSTRUCT ps;
+			HDC hdc = BeginPaint(hWnd, &ps);
+			EndPaint(hWnd, &ps);
+			break;
+		}
+		case WM_DESTROY:
+		{
+			PostQuitMessage(0);
+			break;
+		}
+		default:
+		{
+			return DefWindowProc(hWnd, message, wParam, lParam);
+			break;
+		}
+	}
+	return 0;
+}
